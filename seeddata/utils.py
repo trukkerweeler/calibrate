6910e152ea049b83ed6cdefde45088edffc8e3fa
trukkerweeler <ken22008@byui.edu>
2025-03-27 04:37:17 -0600

    devices page

--- seeddata/utils.py
+++ seeddata/utils.py
@@ -1,12 +1,12 @@
 import os
 from datetime import datetime, timedelta
 from icecream import ic
-from dotenv.main import load_dotenv
-load_dotenv()
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
+# from dotenv.main import load_dotenv
+# load_dotenv()
+# DB_HOST = os.getenv('DB_HOST')
+# DB_NAME = os.getenv('DB_NAME')
+# DB_USER = os.getenv('DB_USER')
+# DB_PASS = os.getenv('DB_PASS')
 
 
 def getcomputername():
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 // const exp = require("constants");
 const cors = require("cors");
